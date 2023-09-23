981ab915624f984fad1029b1501e04a72cfed484
Michal Altair Valášek <github@rider.cz>
2023-09-23 15:20:42 +0200

    Add support for generating cut marks.

--- Commands/MergeCommand.cs
+++ Commands/MergeCommand.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 
 namespace Fortuna.Commands;
 
-[Command("merge", "Merge multiple images into single file")]
+[Command("merge", Description = "Merge multiple images into single file")]
 internal class MergeCommand {
 
     [Argument(0, "source-folder", "Folder containing images to merge, created by `generate` command.")]
@@ -23,6 +24,12 @@ internal class MergeCommand {
     [Argument(3, "rows", "How many images is to be merged horizontally.")]
     [Required, Range(1, 100)]
     public int Rows { get; set; }
+
+    [Option("--cut-marks", Description = "Draw cut marks on the page.", ShortName = "cm")]
+    public bool CutMarks { get; set; } = false;
+
+    [Option("--cut-mark-length <length>", Description = "Length of the cut marks in mm.", ShortName = "cml")]
+    public int CutMarkLength { get; set; } = 5;
 
     [Option("--dpi <number>", Description = "Set the DPI resolution of the generated images.")]
     public int Dpi { get; set; } = 300;
@@ -55,6 +62,8 @@ internal class MergeCommand {
             pageImage.Metadata.ResolutionUnits = SixLabors.ImageSharp.Metadata.PixelResolutionUnit.PixelsPerInch;
             pageImage.Metadata.VerticalResolution = this.Dpi;
             pageImage.Metadata.HorizontalResolution = this.Dpi;
+
+            // Add images
             for (var r = 0; r < this.Rows; r++) {
                 for (var c = 0; c < this.Columns; c++) {
                     Console.Write($"  #{i,4} [{p,3},{c,3},{r,3}] ");
@@ -70,6 +79,29 @@ internal class MergeCommand {
                     i++;
                 }
             }
+
+            // Add horizontal cut marks
+            var cmsPixels = (int)Math.Round(this.CutMarkLength * this.Dpi / 25.4);
+            for (var r = 1; r < this.Rows; r++) {
+                var y = r * testImage.Height;
+                pageImage.Mutate(i => i.DrawLines(Color.Black, 1, new PointF(0, y), new PointF(cmsPixels, y)));
+                pageImage.Mutate(i => i.DrawLines(Color.Black, 1, new PointF(pageImageWidth - cmsPixels, y), new PointF(pageImageWidth, y)));
+                for (var c = 1; c < this.Columns; c++) {
+                    pageImage.Mutate(i => i.DrawLines(Color.Black, 1, new PointF(testImage.Width * c - cmsPixels / 2, y), new PointF(testImage.Width * c + cmsPixels / 2, y)));
+                }
+            }
+
+            // Add vertical cut marks
+            for (var c = 1; c < this.Columns; c++) {
+                var x = c * testImage.Width;
+                pageImage.Mutate(i => i.DrawLines(Color.Black, 1, new PointF(x, 0), new PointF(x, cmsPixels)));
+                pageImage.Mutate(i => i.DrawLines(Color.Black, 1, new PointF(x, pageImageHeight - cmsPixels), new PointF(x, pageImageHeight)));
+                for (var r = 1; r < this.Rows; r++) {
+                    pageImage.Mutate(i => i.DrawLines(Color.Black, 1, new PointF(x, testImage.Height * r - cmsPixels / 2), new PointF(x, testImage.Height * r + cmsPixels / 2)));
+                }
+            }
+
+            // Save page
             var pageFileName = Path.Combine(this.TargetFolder, $"page-{p:0000}{sfi[0].Extension}");
             Console.Write($"Saving {pageFileName}...");
             pageImage.Save(pageFileName);
